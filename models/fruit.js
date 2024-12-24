62eb73f55936a0a7db30fe1d3d46292564b820f4
Lulwa Alsayegh <Lulwa.alsayegh@gmail.com>
2024-12-24 18:56:21 +0300

    test

--- models/fruit.js
+++ models/fruit.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 
 
 //1. create the schema
-const fruitSchema = new mangoose.Schema({
+const fruitSchema = new mongoose.Schema({
     name: String,
     isReadyToEat: Boolean
 })
 
 //2. register the model
-const Fruit = mangoose.model('Fruit', fruitSchema)
+const Fruit = mongoose.model('Fruit', fruitSchema)
 
 //3. share it with the rest of the app
 module.exports = Fruit
--- server.js
+++ server.js
@@ -4,19 +4,49 @@ require('./config/database')
 const express = require('express');
 const morgan = require('morgan');
 
+
 //Models
 const Fruit = require("./models/fruit.js");
 
+
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+
 
 // MIDDLEWARE
 app.use(morgan('dev'));
+
 
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
   });  
-  
+
+
+// GET /fruits/new
+
+
+app.get('/fruits', async (req, res) => {
+  const allFruits = await Fruit.find()
+  res.render('fruits/index.ejs', { fruits: allFruits})
+})
+
+app.get("/fruits/new", (req, res) => {
+    res.render('/fruits/new.ejs')
+  });
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits");
+});
+
+
 // ROUTES
 app.listen(3000, () => {
   console.log('Listening on port 3000');
